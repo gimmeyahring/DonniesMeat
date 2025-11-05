c8cae7bd5843c68d44a6b6a3405bd8b3e094a61e
1155381 <donavanriddick08@gmail.com>
2025-11-05 14:52:37 -0500

    donnies meat caught up

---
+++ app/src/main/java/org/baltimorecityschools/donniesmeatsyeah/MainActivity.java
@@ -0,0 +1,42 @@
+package org.baltimorecityschools.donniesmeatsyeah;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button doneBTN;
+    EditText enterUsername;
+    String username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        enterUsername = (EditText) findViewById(R.id.setUsername);
+        doneBTN = (Button) findViewById(R.id.donebut);
+
+        doneBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = enterUsername.getText().toString();
+                Intent leaveIntent;
+                leaveIntent = new Intent(MainActivity.this, MenuActivity.class);
+                leaveIntent.putExtra("usernameExtra", username);
+                startActivity(leaveIntent);
+            }
+        });
+
+
+    }
+}
